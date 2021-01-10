255fda0276250e2576a9ac4a7127202c77abc02a
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-09 16:40:28 -0800

    removed hard coded connection node-postgres

--- pern/server/db/index.js
+++ pern/server/db/index.js
@@ -1,12 +1,6 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'josephalmeida',
-  host: 'localhost',
-  database: 'yelper',
-  password: null,
-  port: 5432,
-});
+const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 }
