c61d08eb2ba611219a4fb8dd215c5e311c329dc8
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-10 00:51:24 -0800

    Routing, importing, exporting

--- pern/client/src/App.jsx
+++ pern/client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './routes/Home';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdatePage from './routes/UpdatePage';
+
 
 const App = () => {
-  return <h1>HELLO WORLD!</h1>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/restaurant/:id' component={RestaurantDetailPage} />
+          <Route exact path='/restaurant/:id/update' component={UpdatePage} />
+        </Switch>
+
+      </Router>
+    </div>
+  )
 }
 
 export default App;
