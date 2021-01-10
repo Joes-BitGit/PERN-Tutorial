810f36926c4798842a57302588211de68a118ba8
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-10 00:51:48 -0800

    Functional component pages

---
+++ pern/client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantDetailPage = () => {
+  return (
+    <div>
+      DETAIL PAGE
+    </div>
+  )
+}
+
+export default RestaurantDetailPage
---
+++ pern/client/src/routes/UpdatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdatePage = () => {
+  return (
+    <div>
+      UPDATE PAGE
+    </div>
+  )
+}
+
+export default UpdatePage
