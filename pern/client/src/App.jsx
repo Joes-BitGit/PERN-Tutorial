3bb9368994d81a164dd17c099bdc5e1974c9547b
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-10 18:06:53 -0800

    Context API

--- pern/client/src/App.jsx
+++ pern/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 import Home from './routes/Home';
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
 import UpdatePage from './routes/UpdatePage';
@@ -7,16 +8,19 @@ import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/restaurant/:id' component={RestaurantDetailPage} />
-          <Route exact path='/restaurant/:id/update' component={UpdatePage} />
-        </Switch>
+    <RestaurantsContextProvider>
+      <div className='container'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/restaurant/:id' component={RestaurantDetailPage} />
+            <Route exact path='/restaurant/:id/update' component={UpdatePage} />
+          </Switch>
 
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+
   )
 }
 
