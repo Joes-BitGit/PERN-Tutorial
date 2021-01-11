3d466e0e17b10e6b3f35dc7b09162d819f6dcdb5
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-10 18:06:43 -0800

    home page layout

---
+++ pern/client/src/components/RestaurantList.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const RestaurantList = () => {
+  return (
+    <div className='list-group'>
+      <table className="table table-dark table-hover">
+        <thead >
+          <tr className='bg-primary'>
+            <th scope='col'>Restaurant</th>
+            <th scope='col'>Location</th>
+            <th scope='col'>Price Range</th>
+            <th scope='col'>Reviews</th>
+            <th scope='col'>Edit</th>
+            <th scope='col'>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Pho</td>
+            <td>Cerritos</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td><button className="btn btn-warning">Edit</button></td>
+            <td><button className="btn btn-danger">Delete</button></td>
+          </tr>
+          <tr>
+            <td>Pho</td>
+            <td>Cerritos</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td><button className="btn btn-warning">Edit</button></td>
+            <td><button className="btn btn-danger">Delete</button></td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div >
+  )
+}
+
+export default RestaurantList
--- pern/client/src/routes/Home.jsx
+++ pern/client/src/routes/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import AddRestaurant from '../components/AddRestaurant';
+import Header from '../components/Header';
+import RestaurantList from '../components/RestaurantList';
 
 const Home = () => {
   return (
     <div>
-      HOMEPAGE
+      <Header />
+      <AddRestaurant />
+      <RestaurantList />
     </div>
   )
 }
