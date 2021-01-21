db9d9a5ae64ebe883c251086e590384598994d61
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-20 19:21:18 -0800

    className edit

--- pern/client/src/components/StarRating.jsx
+++ pern/client/src/components/StarRating.jsx
@@ -6,11 +6,11 @@ const StarRating = (props) => {
   for (let index = 1; index <= 5; index++) {
     // Fill Star
     if (index <= props.rating) {
-      stars.push(<i class="fas fa-star text-warning"></i>);
+      stars.push(<i className="fas fa-star text-warning"></i>);
     } else if (index === Math.ceil(props.rating) && !Number.isInteger(props.rating)) {
-      stars.push(<i class="fas fa-star-half-alt text-warning"></i>)
+      stars.push(<i className="fas fa-star-half-alt text-warning"></i>)
     } else {
-      stars.push(<i class="far fa-star text-warning"></i>);
+      stars.push(<i className="far fa-star text-warning"></i>);
     }
   }
   return (
