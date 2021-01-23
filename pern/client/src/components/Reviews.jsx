39c45e769ab419588a4a811460501e8d2ca8365c
Joes-BitGit <josephnalmeida@gmail.com>
2021-01-22 17:33:57 -0800

    Removed comments of static code

--- pern/client/src/components/Reviews.jsx
+++ pern/client/src/components/Reviews.jsx
@@ -17,51 +17,6 @@ const Reviews = (props) => {
           </div>
         );
       })}
-      {/* <div className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
-        <div className="card-header d-flex justify-content-between">
-          <span>Joan</span>
-          <span><StarRating rating={4.2} /></span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">Great Restaurant</p>
-        </div>
-      </div>
-      <div className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
-        <div className="card-header d-flex justify-content-between">
-          <span>Joan</span>
-          <span><StarRating rating={4.2} /></span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">Great Restaurant</p>
-        </div>
-      </div>
-      <div className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
-        <div className="card-header d-flex justify-content-between">
-          <span>Joan</span>
-          <span><StarRating rating={4.2} /></span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">Great Restaurant</p>
-        </div>
-      </div>
-      <div className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
-        <div className="card-header d-flex justify-content-between">
-          <span>Joan</span>
-          <span><StarRating rating={4.2} /></span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">Great Restaurant</p>
-        </div>
-      </div>
-      <div className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
-        <div className="card-header d-flex justify-content-between">
-          <span>Joan</span>
-          <span><StarRating rating={4.2} /></span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">Great Restaurant</p>
-        </div>
-      </div> */}
     </div>
   )
 }
